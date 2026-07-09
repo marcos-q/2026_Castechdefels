6fdbd8d44178212c2a92edb048b0e9f3e2c38a0d
marcos <marcos.quilez@upc.edu>
2026-07-09 09:20:24 +0200

    Deployed 19ac27a with MkDocs version: 1.6.1

--- Exemples/ESP_Telegram_base/WIFI_config.h
+++
@@ -1,8 +0,0 @@
-const char* ssid     = "castechdefels2025";
-const char* password = "Castech2024$";
-
-const char* hostname = "ESP32_1";
-
-IPAddress ip(192, 168, 1, 200);
-IPAddress gateway(192, 168, 1, 1);
-IPAddress subnet(255, 255, 255, 0);
